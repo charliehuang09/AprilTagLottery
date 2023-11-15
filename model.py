16997e9fd313444e5e3a3ba07f275b5b3aa5b256
charliehuang09 <41605449+charliehuang09@users.noreply.github.com>
2023-11-14 17:44:41 -0800

    Made size(channels) adjustable for U-net

--- model.py
+++ model.py
@@ -3,66 +3,66 @@ import torch.nn as nn
 from torchvision.transforms import CenterCrop
 
 class Unet(torch.nn.Module):
-    def __init__(self, channels=4):
+    def __init__(self, channels):
         super().__init__()
         self.activation = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
         #first block
-        self.conv1 = nn.Conv2d(3, 64, (3, 3))
-        self.conv2 = nn.Conv2d(64, 64, (3, 3))
+        self.conv1 = nn.Conv2d(3, channels * 1, (3, 3))
+        self.conv2 = nn.Conv2d(channels * 1, channels * 1, (3, 3))
         #first max pool
         self.maxpool3 = nn.MaxPool2d((2,2))
 
         #second block
-        self.conv4 = nn.Conv2d(64, 128, (3,3))
-        self.conv5 = nn.Conv2d(128, 128, (3,3))
+        self.conv4 = nn.Conv2d(channels * 1, channels * 2, (3,3))
+        self.conv5 = nn.Conv2d(channels * 2, channels * 2, (3,3))
         #second max pool
         self.maxpool6 = nn.MaxPool2d((2,2))
 
         #third block
-        self.conv7 = nn.Conv2d(128, 256, (3,3))
-        self.conv8 = nn.Conv2d(256, 256, (3,3))
+        self.conv7 = nn.Conv2d(channels * 2, channels * 4, (3,3))
+        self.conv8 = nn.Conv2d(channels * 4, channels * 4, (3,3))
         #third max pool
         self.maxpool9 = nn.MaxPool2d((2,2))
 
         #forth block
-        self.conv10 = nn.Conv2d(256, 512, (3, 3))
-        self.conv11 = nn.Conv2d(512, 512, (3,3))
+        self.conv10 = nn.Conv2d(channels * 4, channels * 8, (3, 3))
+        self.conv11 = nn.Conv2d(channels * 8, channels * 8, (3,3))
         #forth max pool
         self.maxpool12 = nn.MaxPool2d((2,2))
 
         #-------------------------------------------
 
-        self.conv13 = nn.Conv2d(512, 1024, (3,3))
-        self.conv14 = nn.Conv2d(1024, 1024, (3,3))
+        self.conv13 = nn.Conv2d(channels * 8, channels * 16, (3,3))
+        self.conv14 = nn.Conv2d(channels * 16, channels * 16, (3,3))
         
         #-------------------------------------------
 
         #first block
-        self.up15 = nn.ConvTranspose2d(1024, 512, (2,2), 2)
+        self.up15 = nn.ConvTranspose2d(channels * 16, channels * 8, (2,2), 2)
         #cat
-        self.conv16 = nn.Conv2d(1024, 512, (3,3))
-        self.conv17 = nn.Conv2d(512, 512, (3,3))
+        self.conv16 = nn.Conv2d(channels * 16, channels * 8, (3,3))
+        self.conv17 = nn.Conv2d(channels * 8, channels * 8, (3,3))
 
         #second block
-        self.up18 = nn.ConvTranspose2d(512, 256, (2,2), 2)
+        self.up18 = nn.ConvTranspose2d(channels * 8, channels * 4, (2,2), 2)
         #cat
-        self.conv19 = nn.Conv2d(512, 256, (3,3))
-        self.conv20 = nn.Conv2d(256, 256, (3,3))
+        self.conv19 = nn.Conv2d(channels * 8, channels * 4, (3,3))
+        self.conv20 = nn.Conv2d(channels * 4, channels * 4, (3,3))
 
         #third block
-        self.up21 = nn.ConvTranspose2d(256, 128, (2,2), 2)
+        self.up21 = nn.ConvTranspose2d(channels * 4, channels * 2, (2,2), 2)
         #cat
-        self.conv22 = nn.Conv2d(256, 128, (3,3))
-        self.conv23 = nn.Conv2d(128, 128, (3,3))
+        self.conv22 = nn.Conv2d(channels * 4, channels * 2, (3,3))
+        self.conv23 = nn.Conv2d(channels * 2, channels * 2, (3,3))
 
         #forth block
-        self.up24 = nn.ConvTranspose2d(128, 64, (2,2), 2)
+        self.up24 = nn.ConvTranspose2d(channels * 2, channels * 1, (2,2), 2)
         #cat
-        self.conv25 = nn.Conv2d(128, 64, (3,3))
-        self.conv26 = nn.Conv2d(64, 64, (3,3))
-        self.conv27 = nn.Conv2d(64, 1, (1,1))
+        self.conv25 = nn.Conv2d(channels * 2, channels * 1, (3,3))
+        self.conv26 = nn.Conv2d(channels * 1, channels * 1, (3,3))
+        self.conv27 = nn.Conv2d(channels * 1, 1, (1,1))
     
     def forward(self, x):
         #first block
